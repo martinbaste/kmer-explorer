5ef4d791638d97eb26697e5de4abca5ea48514f3
Martín Basterrechea <martinbaste@users.noreply.github.com>
2016-04-26 11:53:44 +0200

    rename files to random id and return it
    add gitignore

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,20 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
-
+var uuid = require('node-uuid');
 
 router.post('/fileupload', function(req, res) {
     var fstream;
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
         console.log("Uploading: " + filename);
-        var saveTo = path.join(__dirname, '../files/'+filename);
+        var id = uuid.v4();
+        console.log(id)
+        var saveTo = path.join(__dirname, '../files/'+id + '.fastq');
         fstream = fs.createWriteStream(saveTo);
         file.pipe(fstream);
         fstream.on('close', function () {
-            res.redirect('back');
+            res.render('processing', {id: id});
         });
     });
 });
